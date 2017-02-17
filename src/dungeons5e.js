6b794345c4fd5f0a954fbccc73d081f4ce1d24ea
Jono Kemball <jonathankemball@gmail.com>
2017-02-17 23:18:46 +1300

    Added TopNavigation UI component

--- src/dungeons5e.js
+++ src/dungeons5e.js
@@ -1,52 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react';
+import {AppRegistry, Text, View, StatusBar} from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {TopNavigation} from './common/components/TopNavigation'
+
+var StyleSheet = require('./common/XStyleSheet');
 
 export default class Dungeons5e extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Dungeons5e!
-        </Text>
-        <Text style={styles.instructions}>
-          Fuck how do I write actual code kms
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TopNavigation name="D&D 5e" statusColor="#d32f2f" menuColor=""/>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit dungeons5e.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Double tap R on your keyboard to reload,{'\n'}
+                    Shake or press menu button for dev menu
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#fefefe',
-  },
-  welcome: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'left',
-    color: '#666666',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    welcome: {
+        fontSize: 30,
+        textAlign: 'center',
+        margin: 10,
+        color: "#000"
+    },
+    instructions: {
+        textAlign: 'left',
+        color: '#000',
+        marginBottom: 5
+    }
 });
 
 AppRegistry.registerComponent('Dungeons5e', () => Dungeons5e);
